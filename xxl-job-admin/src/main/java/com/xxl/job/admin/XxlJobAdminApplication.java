ba13e2f330cb6c59d67d018704160fa9440e82f0
chen <41614969@qq.com>
2020-04-26 17:49:04 +0800

    初始化第一版 采用xxljob2.2.0版本    增加打包和启动,关闭sh

---
+++ xxl-job-admin/src/main/java/com/xxl/job/admin/XxlJobAdminApplication.java
@@ -0,0 +1,17 @@
+package com.xxl.job.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * http://127.0.0.1:8080/xxl-job-admin/
+ * @author xuxueli 2018-10-28 00:38:13
+ */
+@SpringBootApplication
+public class XxlJobAdminApplication {
+
+	public static void main(String[] args) {
+        SpringApplication.run(XxlJobAdminApplication.class, args);
+	}
+
+}
